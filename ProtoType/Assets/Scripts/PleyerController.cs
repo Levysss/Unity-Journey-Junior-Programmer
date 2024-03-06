53a12d5f23fa841101e2cd8d986c1f4313174c37
Matheus Levy <srlevy008@gmail.com>
2024-03-06 16:42:34 -0300

    1.2.6

---
+++ ProtoType/Assets/Scripts/PleyerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PleyerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //we'll move vihicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime*20);
+    }
+}
